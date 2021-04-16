01966f56bb336d8defe3494e16ad040525fc6a9e
JooHyeongWoo <j89003145@gmail.com>
2021-04-16 18:12:33 +0900

    카메라 눈검출 잘안되는중

--- eyedectcamera.py
+++ eyedectcamera.py
@@ -1,18 +1,92 @@
-import cv2 
+import cv2, dlib
 import numpy as np
 from imutils import face_utils
 
+
+
+IMG_SIZE = (34, 26)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('blink_eyes_detection_prevention_eyedisease/shape_predictor_68_face_landmarks.dat')
+
+RIGHT_EYE = list(range(36, 42))  
+LEFT_EYE = list(range(42, 48)) 
+index = LEFT_EYE + RIGHT_EYE
+
+
+
+def crop_eye(img, eye_points):
+  x1, y1 = np.amin(eye_points, axis=0)
+  x2, y2 = np.amax(eye_points, axis=0)
+  cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
+
+  w = (x2 - x1) * 1.2
+  h = w * IMG_SIZE[1] / IMG_SIZE[0]
+
+  margin_x, margin_y = w / 2, h / 2
+
+  min_x, min_y = int(cx - margin_x), int(cy - margin_y)
+  max_x, max_y = int(cx + margin_x), int(cy + margin_y)
+
+  eye_rect = np.rint([min_x, min_y, max_x, max_y]).astype(np.int)
+
+  eye_img = gray[eye_rect[1]:eye_rect[3], eye_rect[0]:eye_rect[2]]
+
+  return eye_img, eye_rect
+
+# main
+#cap = cv2.VideoCapture('blink_eyes_detection_prevention_eyedisease/testeyedection/eyewithmask.mp4')
+#cap = cv2.VideoCapture('blink_eyes_detection_prevention_eyedisease/testeyedection/eyewithoutmask.mp4')
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-while(True):
-    ret, cam = cap.read()
+while cap.isOpened():
+  ret, img_ori = cap.read()
 
-    if(ret) :
-        cv2.imshow('camera', cam)
-        
-        
-        if cv2.waitKey(1) & 0xFF == 27: 
-            break
-                     
-cap.release()
-cv2.destroyAllWindows()
+  if not ret:
+    break
+
+  img_ori = cv2.resize(img_ori, dsize=(0, 0), fx=0.8, fy=0.8)
+
+  img = img_ori.copy()
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  faces = detector(gray)
+
+  for face in faces:
+    shapes = predictor(gray, face)
+    shapes = face_utils.shape_to_np(shapes)
+
+    eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
+    eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
+
+    eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+    eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
+    eye_img_r = cv2.flip(eye_img_r, flipCode=1)
+
+    cv2.imshow('l', eye_img_l)
+    cv2.imshow('r', eye_img_r)
+
+    eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+    eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+ 
+    shape = predictor(gray, face)
+    list_points = []
+    for p in shape.parts():
+        list_points.append([p.x, p.y])
+
+    list_points = np.array(list_points)
+
+
+    for i,pt in enumerate(list_points[index]):
+
+        pt_pos = (pt[0], pt[1])
+        cv2.circle(img, pt_pos, 2, (0, 255, 0), -1)
+
+
+    #cv2.rectangle(img, pt1=tuple(eye_rect_l[0:2]), pt2=tuple(eye_rect_l[2:4]), color=(255,255,255), thickness=2)
+    #cv2.rectangle(img, pt1=tuple(eye_rect_r[0:2]), pt2=tuple(eye_rect_r[2:4]), color=(255,255,255), thickness=2)
+
+
+  cv2.imshow('result', img)
+  if cv2.waitKey(1) == ord('q'):
+    break
